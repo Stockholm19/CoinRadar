64aff0bbff080e01ce5267f081eb273361827290
Abrikozs <Abrikozs@gmail.com>
2025-03-20 20:03:12 +0300

    Added the loading of detailed coin information in DetailView
    - Created `DetailViewModel`, which controls the loading and processing of data for` DetailView`
    - Connected `Coindetaildataservice` to obtain additional information about the coin
    - Subscription for data and updating statistics in `DetailViewModel` was implemented
    - Added overview and accessal statistics, including price, capitalization, rank and hashing algorithm

--- CoinRadar/Core/Components/Detail/View/DetailView.swift
+++ CoinRadar/Core/Components/Detail/View/DetailView.swift
@@ -8,18 +8,88 @@
 import SwiftUI
 
 struct DetailView: View {
-    @StateObject var vm: DetailViewModel
+    @StateObject private var vm: DetailViewModel
+    private let columns: [GridItem] = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+    private let spacing: CGFloat = 30.0
+        
     
     init(coin: CoinModel) {
         _vm = StateObject(wrappedValue: DetailViewModel(coin: coin))
-        print("Initializing Detail View for: \(coin.name ?? "N/A")")
+//        print("Initializing Detail View for: \(coin.name ?? "N/A")")
     }
     
     var body: some View {
-        Text("Hello")
+        ScrollView {
+            VStack(spacing: 20.0) {
+                Text("")
+                    .frame(height: 150)
+                
+                overviewTitle
+                Divider()
+                overviewGrid
+                additionalTitle
+                Divider()
+                additionalGrid
+            }
+            .padding()
+        }
+        .navigationTitle(vm.coin.name ?? "")
     }
 }
 
 #Preview {
-    DetailView(coin: dev.coin)
+    NavigationStack {
+        DetailView(coin: dev.coin)
+    }
 }
+
+extension DetailView {
+    
+    private var overviewTitle: some View {
+        Text("Overview")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var additionalTitle: some View {
+        Text("Additional Details")
+            .font(.title)
+            .bold()
+            .foregroundColor(Color.theme.accent)
+            .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var overviewGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(vm.overviewStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
+        )
+    }
+    
+    private var additionalGrid: some View {
+        LazyVGrid(
+            columns: columns,
+            alignment: .leading,
+            spacing: spacing,
+            pinnedViews: [],
+            content: {
+                ForEach(vm.additionalStatistics) { stat in
+                    StatisticView(stat: stat)
+                }
+            }
+        )
+    }
+}
+
